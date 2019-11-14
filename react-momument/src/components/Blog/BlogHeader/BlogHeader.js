7f7e8e79e16e09a8b6b406e524bc74557e47fbb9
KennyK-TW <b2khakashu@yahoo.com>
2019-11-13 21:57:56 -0800

    class to className

--- react-momument/src/components/Blog/BlogHeader/BlogHeader.js
+++ react-momument/src/components/Blog/BlogHeader/BlogHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BlogHeader = () => {
     return (
-        <header class="vertical-center">
+        <header className="vertical-center">
             <div>
                 <h1><a href="index.html">Monument</a></h1>
             </div>
